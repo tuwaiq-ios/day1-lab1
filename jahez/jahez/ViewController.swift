9d655db80d058874696e0ff31c42e1ee6faf9319
PC <alahmryn5@gmail.com>
2021-09-27 12:05:38 +0300

    this is my first lab

---
+++ jahez/jahez/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jahez
+//
+//  Created by PC on 20/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
