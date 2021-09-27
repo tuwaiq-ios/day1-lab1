d1644c729c5d2e86f9712657bef546e268f21ba9
Afnan khalid <Afnanmoghram@gmail.com>
2021-09-27 16:31:23 +0300

    this is my first lab

---
+++ Tuwaiq bootcamp/my first lab/my first lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  my first lab
+//
+//  Created by Fno Khalid on 20/02/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
