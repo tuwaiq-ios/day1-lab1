31b3a00e7f654db52a2978e76b25f100a730868b
HANAN <hanan@HANANs-MacBook-Pro.local>
2021-09-27 09:36:06 +0300

    finish

---
+++ hananibrahim 2/hananibrahim/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  hananibrahim
+//
+//  Created by HANAN on 19/02/1443 AH.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
